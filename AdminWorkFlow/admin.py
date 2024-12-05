8c444de84642ccab933fd0eea056d9afceacadd4
Ajay <jaykumar002kori@gmail.com>
2024-12-05 16:42:17 +0530

    commit

--- AdminWorkFlow/admin.py
+++ AdminWorkFlow/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin.sites import site
+from AdminWorkFlow.models import indexdetails
 
-# Register your models here.
+## Admin work
+
+class indexdetailsAdmin(admin.ModelAdmin):
+    list_display = ('course_title', 'course_description', 'course_thumbnail')
+    
+
+admin.site.register(indexdetails, indexdetailsAdmin)    
