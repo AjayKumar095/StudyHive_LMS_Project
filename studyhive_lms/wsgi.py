b031231dea5a862f89382f137b85af572f8dbe96
Ajay <jaykumar002kori@gmail.com>
2024-12-18 16:40:17 +0530

    commit

--- studyhive_lms/wsgi.py
+++ studyhive_lms/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+# Add the project directory to sys.path (the parent directory of this wsgi.py file)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studyhive_lms.settings")
