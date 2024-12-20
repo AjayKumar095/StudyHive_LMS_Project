130945aa48300f2bd6da1b3b92beaf398fc23dad
Ajay <jaykumar002kori@gmail.com>
2024-12-20 18:34:51 +0530

    commit

--- studyhive_lms/views.py
+++ studyhive_lms/views.py
@@ -294,7 +294,7 @@ def course_view(request):
             return render(request=request, template_name="course_content_view.html",context=course_content)            
     
     except Exception as e:
-        messages.error(request=request, message=e)
+        messages.error(request=request, message="Not enter in course_view {e}")
         return redirect('mycourses')
 
 ## Assignment submit module
