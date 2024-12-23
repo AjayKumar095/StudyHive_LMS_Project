657ea75062f18a45dc912922df2c8da935c4a8e9
Ubuntu <jaykumar002kori@gmail.com>
2024-12-23 12:29:56 +0000

    commit

--- studyhive_lms/settings.py
+++ studyhive_lms/settings.py
@@ -144,7 +144,7 @@ LOGIN_URL = '/user/'  # Set your custom login path
 # Email setup for development 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "futureteck71@gmail.com"
 EMAIL_HOST_PASSWORD = "nrlpdlqegwlouayc"
